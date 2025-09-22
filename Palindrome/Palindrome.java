db3a2d63eff033550e95749c47cc264b5846b082
fahdaguenouz <faguenouz@gmail.com>
2025-09-22 12:34:25 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s==null){
+            return null;
+        }
         String reverse="";
         for (int i=s.length()-1;i>=0;i--){
             char letter = s.charAt(i);
