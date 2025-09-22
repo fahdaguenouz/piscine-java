296f52bc336ab1d0e4688ff16ba5cf543264ab48
fahdaguenouz <faguenouz@gmail.com>
2025-09-22 11:40:14 +0100

    StringContains/

---
+++ StringContains/StringContains.java
@@ -0,0 +1,9 @@
+public class StringContains {
+    public static boolean isStringContainedIn(String subString, String s) {
+      if (s.contains(subString)){
+        return true;
+      }else{
+        return false;
+      }
+    }
+}
