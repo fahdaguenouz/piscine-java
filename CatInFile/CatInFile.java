3ec6e20314231dbe8234c0bff411ea346e182f97
fahdaguenouz <faguenouz@gmail.com>
2025-09-23 11:49:49 +0100

    Catinfile

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -1,24 +1,19 @@
 import java.io.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CatInFile {
     public static void cat(String[] args) throws IOException {
-        if (args.length == 0) {
-            return; 
+ if (args.length !=1){
+            return ;
         }
-
-        String filename = args[0];
-
-        try (
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter writer = Files.newBufferedWriter(Path.of(filename))
-        ) {
-            int c;
-            while ((c = reader.read()) != -1) {  
-                writer.write(c);
-            }
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
-        }
+        String filename= args[0];
+        Path path= Path.of(filename);
+        try {
+            byte[] content = System.in.readAllBytes();
+            Files.write(path,content);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } 
     }
-}
+}   
