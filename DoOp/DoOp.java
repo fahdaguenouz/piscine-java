8da703f794837756321374a73c830252d3cb8940
fahdaguenouz <faguenouz@gmail.com>
2025-09-23 10:56:37 +0100

    DoOp

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,7 +1,7 @@
 public class DoOp {
     public static String operate(String[] args) {
         if(args.length==0){
-            return "it depend on the input.";
+            return "Error";
         }
        int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[2]);
