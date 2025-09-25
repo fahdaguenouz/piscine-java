b678001ece09192b3f770caedf4934db91b4518a
fahdaguenouz <faguenouz@gmail.com>
2025-09-25 11:22:42 +0100

    StarGetters

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,48 @@
+
+public class CelestialObject {
+  public double x;
+  public double y;
+  public double z;
+  public String name;
+
+  public CelestialObject() {
+    this.x = 0.0;
+    this.y = 0.0;
+    this.z = 0.0;
+    this.name = "Soleil";
+  }
+  public CelestialObject(String name, double x, double y, double z) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+   public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
