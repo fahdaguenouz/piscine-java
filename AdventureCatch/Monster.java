9124bb1b073a19402b9548cdf5f270bc8d659c6e
fahdaguenouz <faguenouz@gmail.com>
2025-09-29 14:18:08 +0100

    AdventureCatch

---
+++ AdventureCatch/Monster.java
@@ -0,0 +1,25 @@
+public class Monster extends Character {
+
+    public Monster(String name, int maxHealth, Weapon weapon) {
+        super(name, maxHealth, weapon);
+    }
+
+    @Override
+    public void attack(Character target) throws DeadCharacterException {
+        int damage = getWeapon() != null ? getWeapon().getDamage() : 7;
+        target.takeDamage(damage);
+    }
+
+    @Override
+    public void takeDamage(int damage) throws DeadCharacterException {
+        int actualDamage = (int) Math.floor(damage * 0.8);
+        reduceHealth(actualDamage);
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() > 0)
+            return getName() + " is a monster with " + getCurrentHealth() + " HP";
+        return getName() + " is a monster and is dead";
+    }
+}
---
+++ AdventureCatch/Sorcerer.java
@@ -0,0 +1,37 @@
+public class Sorcerer extends Character implements Healer {
+    private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity, Weapon weapon) {
+        super(name, maxHealth, weapon);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character target) throws DeadCharacterException {
+        target.receiveHealing(healCapacity);
+    }
+
+    @Override
+    public void attack(Character target) throws DeadCharacterException {
+        heal(this);
+        int damage = getWeapon() != null ? getWeapon().getDamage() : 10;
+        target.takeDamage(damage);
+    }
+
+    @Override
+    public void takeDamage(int damage) throws DeadCharacterException {
+        reduceHealth(damage);
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() == 0)
+            return getName() + " is a dead sorcerer. So bad, it could heal " + healCapacity + " HP.";
+        return getName() + " is a sorcerer with " + getCurrentHealth() + " HP. It can heal " + healCapacity + " HP.";
+    }
+}
