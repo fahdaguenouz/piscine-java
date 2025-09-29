1ed12fdbe4b1992be8734353e5882d416675f1b8
fahdaguenouz <faguenouz@gmail.com>
2025-09-29 14:24:04 +0100

    AdventureCatch

--- AdventureCatch/Monster.java
+++ AdventureCatch/Monster.java
@@ -6,6 +6,8 @@ public class Monster extends Character {
 
     @Override
     public void attack(Character target) throws DeadCharacterException {
+        if (getCurrentHealth() == 0) {
+            throw new DeadCharacterException(this); 
         int damage = getWeapon() != null ? getWeapon().getDamage() : 7;
         target.takeDamage(damage);
     }
