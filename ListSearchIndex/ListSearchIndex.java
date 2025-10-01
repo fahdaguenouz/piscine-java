456736173024fecc7e3289af08a3395e45d1e885
fahdaguenouz <faguenouz@gmail.com>
2025-10-01 11:13:12 +0100

    ListSearchIndex

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSearchIndex {
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        int index = list.indexOf(value);
+        return index != -1 ? index : null;
+    }
+
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        int index = list.lastIndexOf(value);
+        return index != -1 ? index : null;
+    }
+
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(value)) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+}
